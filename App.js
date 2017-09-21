6ac50cf3832eb2795d1dd16821fe5b9cd3e462c8
Jorge Marrero <jorgems93@gmail.com>
2017-09-21 17:09:38 +0100

    [react-navigation] Boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		const MainNavigator = TabNavigator({
+			welcome: { screen: WelcomeScreen },
+			auth: { screen: AuthScreen },
+			main: {
+				screen: TabNavigator({
+					map: { screen: MapScreen },
+					deck: { screen: DeckScreen },
+					review: {
+						screen: StackNavigator({
+							review: { screen: ReviewScreen },
+							settings: { screen: SettingsScreen }
+						})
+					}
+				}, {
+					tabBarPosition: 'bottom',
+					lazyLoad: true
+				})
+			}
+		}, {
+			tabBarPosition: 'bottom',
+			lazyLoad: true,
+			swipeEnabled: false,
+			animationEnabled: false
+		}
+		);
+		return (
+			<MainNavigator />
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
