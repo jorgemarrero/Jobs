d81f7d3003b5836df4a4c793235eef9904b418a4
Jorge-SPC\Jorge <jorge.marrero106@alu.ulpgc.es>
2017-09-21 23:15:07 +0100

    Dots on welcome screen

--- components/Slides.js
+++ components/Slides.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { View, Text, ScrollView, Dimensions, Animated } from 'react-native';
 import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -32,16 +32,45 @@ class Slides extends Component {
             );
         });
     }
-    
+
+    renderDots() {
+        const position = Animated.divide(new Animated.Value(0), SCREEN_WIDTH);
+
+        return this.props.data.map((_, i) => { 
+            const opacity = position.interpolate({
+                inputRange: [i - 1, i, i + 1],
+                outputRange: [0.3, 1, 0.3], 
+                extrapolate: 'clamp'
+            });
+            return (
+                <Animated.View
+                    key={i}
+                    style={{ opacity, height: 10, width: 10, backgroundColor: '#595959', margin: 8, borderRadius: 5 }}
+                />
+            );
+        });
+    }
+
     render() {
         return (
-            <ScrollView
-                horizontal
-                pagingEnabled
-                style={{ flex: 1 }}
-            >
-                {this.renderSlides()}
-            </ScrollView>
+            <View>
+                <ScrollView
+                    horizontal
+                    pagingEnabled
+                    style={{ flex: 1 }}
+                    showsHorizontalScrollIndicator={false}
+                    onScroll={Animated.event( 
+                    [{ nativeEvent: { contentOffset: { x: this.scrollX } } }] 
+                    )}
+                    scrollEventThrottle={16} 
+                >
+                    {this.renderSlides()}
+                </ScrollView>
+                
+                <View style={{ flexDirection: 'row' }}>
+                    {this.renderDots()}
+                </View>
+            </View>
         );
     }
 }
